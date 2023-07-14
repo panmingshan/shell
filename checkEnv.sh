5d7077b412fe29d089f97b06e6faa480dfb545ce
panmingshan <1326082784@qq.com>
2023-07-14 21:34:08 +0800

    feat: preinstall

--- checkEnv.sh
+++ checkEnv.sh
@@ -1,8 +1,16 @@
 #! /bin/sh
-
+# 下载目录
+savePath='~/software'
+# 判断是否安装nodejs
 node_env=true;
 if ! type node >/dev/nul 2>&1; then
 node_env=false;
 fi;
 
-echo $node_env
+if $node_env;then
+mkdir -p ~/software
+
+wget -o ~/software/node.tar https://npmmirror.com/mirrors/node/v18.16.1/node-v18.16.1-linux-x64.tar.xz
+cd $savePath
+tar -xvf 
+fi 
