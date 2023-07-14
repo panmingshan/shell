edcf570b52a93e5962c86bfd854bcb4811c70dc5
panmingshan <1326082784@qq.com>
2023-07-15 00:01:02 +0800

    feat: 添加sh文件

--- checkEnv.sh
+++ checkEnv.sh
@@ -14,6 +14,7 @@ mkdir -p $savePath
 
 wget -O "${savePath}/node.tar" https://npmmirror.com/mirrors/node/v18.16.1/node-v18.16.1-linux-x64.tar.xz
 cd $savePath
+mkdir nodejs
 tar -xvf "${savePath}/node.tar" -C 'nodejs'
 cd ./nodejs/bin
 export "$(pwd);$PATH"
