2721159f1aa9bbe9fcc89560f5155f002b1b734e
panmingshan <1326082784@qq.com>
2023-07-15 10:07:13 +0800

    feat: shell

--- checkEnv.sh
+++ checkEnv.sh
@@ -11,12 +11,12 @@ fi;
 #安装nodejs
 if $node_env;then
 mkdir -p $savePath
-
 wget -O "${savePath}/node.tar" https://npmmirror.com/mirrors/node/v18.16.1/node-v18.16.1-linux-x64.tar.xz
 cd $savePath
-mkdir nodejs
-tar -xvf "${savePath}/node.tar" -C 'nodejs'
-nodeBin=$savePath/nodejs/$(ls | grep node)/bin
-cd nodeBin
-export "$(pwd);$PATH"
-fi
+tar -xvf ./node.tar
+nodePath=$(ls | grep node-v)
+mv $nodePath 'nodejs';
+echo ~~~~~~
+ll ./nodejs
+export PATH="$PATH:$(pwd)";
+fi;
