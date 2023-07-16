2f1412975fa90b34b01e98277fa16c0340ed67f3
腾讯云服务name <>
2023-07-16 23:07:52 +0800

    feat: ping

--- ping.sh
+++ ping.sh
@@ -2,18 +2,21 @@
 
 #  检测目标机器是否在线
 
-for ((i=0;i<4;i++));do
+for ((i=1;i<4;i++));do
 if ping -c 1 $1 &>/dev/null;then
-ping_count"$i"=1
+export  ping_count"$i"=1
 else
-ping_count"$i"=0
+export ping_count"$i"=0
 fi
 
 sleep 1;
 done;
 
-if ($ping_count0 + $ping_count1 + $ping_count2) == 3;then
+if [[ $(( $ping_count1 + $ping_count2 + $ping_count3 )) == 3 ]];then
 echo 成功
 else
 echo 失败
 fi;
+unset  ping_count0
+unset  ping_count1
+unset  ping_count2
